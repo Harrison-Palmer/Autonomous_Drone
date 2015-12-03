a515b09b574b4dd64635b0079b8f3c19c79c21b0
Harrison Palmer <HPalmer@student.dwc.edu>
2015-12-03 15:31:26 -0500

    fgxkh

---
+++ WPFLogin-master/FTPImageTransfer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace WpfApp1
+{
+    class FTPImageTransfer
+    {
+
+        public FTPImageTransfer(string address, string login, string password)
+        {
+            Address = address;
+            Login = login;
+            Password = password;
+        }
+
+        public void Upload_2(String path, String name)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Credentials = new NetworkCredential(Login, Password);
+                byte[] b = webClient.UploadFile(Address + "//" + name, "STOR", path);
+                Console.WriteLine(System.Text.Encoding.ASCII.GetString(b));
+            }
+        }
+
+        public string Address { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+}
+
--- WPFLogin-master/Main_Menu.xaml.cs
+++ WPFLogin-master/Main_Menu.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,17 +21,34 @@ namespace WpfApp1
     /// <summary>
     /// Interaction logic for Main_Menu.xaml
     /// </summary>
+    /// 
     public partial class Main_Menu : Window
     {
 
         bool isSearching = false;
+        //String dir_path = @"%USERPROFILE%\\My Documents\\Downloads";
 
         private System.Windows.Forms.MonthCalendar monthCalendar1;
 
         public Main_Menu()
         {
             InitializeComponent();
+            DataContext = this;
+            ImageUri = "user-1-glyph-icon_MkuBPp8O.png";
+            //found_image = "";
         }
+
+        //----Imageuri-start-----\\
+        public static readonly DependencyProperty ImageUriProperty =
+        DependencyProperty.Register("ImageUri", typeof(string), typeof(Main_Menu));
+
+        public string ImageUri
+        {
+            get { return (string)GetValue(ImageUriProperty); }
+            set { SetValue(ImageUriProperty, value); }
+        }
+        //----Imageuri-end-----\\
+
 
         private void Home_Click(object sender, RoutedEventArgs e)
         {
@@ -66,20 +85,46 @@ namespace WpfApp1
         //
         private void Start_Search_Click(object sender, RoutedEventArgs e)
         {
-            if (isSearching)
+            if (!isSearching)
             {
+                //shows continue searching button
                 Continue_Searching.Visibility = Visibility.Visible;
                 Start_Search.Content = "New Search";
-                search_for_image.Fill = new ImageBrush
-                {
-                    ImageSource = new Image("C:\\Users\\hpalmer.LRC\\Documents\\Harrison & Jared & Ryan\\user-1-glyph-icon_MkuBPp8O.png")
-                };
+
+                //makes filepicker window object
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                //Only allows Images
+                openFileDialog.Filter = "Image Files (*.png, *.jpg,*.bmp)|*.png;*.jpg;*.bmp";
+                //sets default directory when opening
+                openFileDialog.InitialDirectory = @"C:\";
+                //Title of image picker window
+                openFileDialog.Title = "Select Image of person to be searched for";
+
+
+
+
+                //idk
+                if (openFileDialog.ShowDialog() == true)
+                   // txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+
+                ImageUri = openFileDialog.FileName;
+
+                FTPImageTransfer transfer = new FTPImageTransfer("192.168.168.1", "drone", "NEVERAGAIN");
+
+                //transfer.Upload_2("", name + DateTime.Now);
             }
             else
             {
                 isSearching = true;
 
             }
+        }
+
+        private void Stop_Button1_Click(object sender, RoutedEventArgs e)
+        {
+            isSearching = false;
+            ImageUri = "user-1-glyph-icon_MkuBPp8O.png";
+            
         }
     }
 }
