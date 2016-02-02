02069c151df88c0ca4844bcd9c71f3c71684195a
Harrison Palmer <HPalmer@student.dwc.edu>
2016-02-02 15:17:29 -0500

    Actually merged me and joes work.

--- WPFLogin-master/Main_Menu.xaml.cs
+++ WPFLogin-master/Main_Menu.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
 using System.Threading.Tasks;
+using System.Diagnostics;
+
 //using System.Windows.Forms;
 
 namespace WpfApp1
@@ -29,27 +31,35 @@ namespace WpfApp1
     public partial class Main_Menu : Window
     {
 
-        bool isSearching = false;
-        string myImage = "C:\\Users\\hpalmer\\Source\\Repos\\Drone_ui\\WPFLogin-master\\user-1-glyph-icon_MkuBPp8O.png";
+        Stopwatch stopwatch = new Stopwatch();
 
+        bool isSearching = false;
+        string myImage = "";
+        //(put in every function that uses it) 
+        //private static UI_Network comms = new UI_Network();
+
+        // Constructor
         public Main_Menu()
         {
             InitializeComponent();
-            Can_Start();
             Safe_to_Fly();
-        }      
+        }
 
+        // Returns to the main menu page.
         private void Home_Click(object sender, RoutedEventArgs e)
         {
             //NavigationService.Navigate(new Uri("test.xaml", UriKind.Relative));
         }
 
         //mouse hover - home
+        //TODO - remove and put in designer properties/XAML
         private void Home_MouseEnter(object sender, MouseEventArgs e)
         {
             this.Cursor = Cursors.Hand;
         }
 
+        // Closes the application window, logs current user out, 
+        // then re-opens login window.
         private void Logout_Click(object sender, RoutedEventArgs e)
         {
             var newW = new login();
@@ -58,91 +68,125 @@ namespace WpfApp1
             this.Close();
         }
 
-        //
+        // Send signal to drone to commence searching for the selected target.
+        //TODO - handle new search while already searching 
         private void Start_Search_Click(object sender, RoutedEventArgs e)
         {
-            //resets the upload fail/pass dialog box
-            Upload_status.Content = "";
+            // Resets the upload fail/pass dialog box
+            status_box.Foreground = Brushes.Yellow;
+            status_box.Content = "User Selecting target. ";
+
+            status_label.Foreground = Brushes.Yellow;
+            status_label.Content = "Standby";
 
             FTPImageTransfer transfer = null;
+
+            // Begins the drone search process by having the user select a image of
+            // the target.
             if (!isSearching)
             {
-                //shows continue searching button
+                // Shows continue searching button.
                 Continue_Searching.Visibility = Visibility.Visible;
                 Start_Search.Content = "New Search";
 
-                //makes filepicker window object
-                OpenFileDialog openFileDialog = new OpenFileDialog();
-                //Only allows Images
+                // Makes filepicker window object.
+                var openFileDialog = new OpenFileDialog();
+                // Only allows Images.
                 openFileDialog.Filter = "Image Files (*.png, *.jpg,*.bmp)|*.png;*.jpg;*.bmp";
                 openFileDialog.FilterIndex = 1;
-                //sets default directory when opening
+                // Sets default directory when opening.
                 openFileDialog.InitialDirectory = @"C:\";
-                //Title of image picker window
+                // Title of image picker window.
                 openFileDialog.Title = "Select Image of person to be searched for";
 
                 bool? click_ok = openFileDialog.ShowDialog();
-                
-                //sets variable myImage to the uploaded image
+
+                // Sets variable myImage to the uploaded image.
                 myImage = openFileDialog.FileName;
-                //sets image box to the image path in myImage
+                // Sets image box to the image path in myImage.
                 search_for_image.Source = setImage();
-                
+
+                // Attemps to send user-selected image to server for drone search.
                 try
                 {
                     if (click_ok == true)
                     {
+                        // Makes a new FTP connection to the server.
                         transfer = new FTPImageTransfer("ftp://192.168.168.1", "drone", "NEVERAGAIN");
-                        string data = "";//DateTime.Now.ToString();
-                        string name = "ui_image" + data + ".png"; // + DateTime.Now;
-                                                                  //uploads image with name of ui_Image and the date
+                        // Uses DateTime.Now.ToString();.
+                        var data = "";
+                        // Uploads image with name of ui_Image and the date.
+                        var name = "ui_image" + data + ".png";
 
+                        // Sends image file to server.
                         transfer.Upload(openFileDialog.FileName, name);
-                        //comms.SendImage(name);
 
-                        Upload_status.Foreground = Brushes.Green;
-                        Upload_status.Content = "Image succesfully uploaded.";
-                    }
-                    else
-                    {
-                        isSearching = true;
+                        try
+                        {
+                            //comms.SendImage(name);
+                            //  comms.SendStart();
+                        }
+                        catch
+                        {
+
+                        }
+
+                        // Sets Dialog box's color and text with respect to the image upload result
+                        status_box.Foreground = Brushes.Green;
+                        if (Start_Search.Content == "New Search")
+                            status_box.Content = "Image succesfully uploaded, Searching for new target. ";
+                        else
+                            status_box.Content = "Image succesfully uploaded, Drone starting. ";
+
+                        status_label.Foreground = Brushes.Green;
+                        status_label.Content = "Active";
+
+                        // Starts the timer, if user picks new target timer does not reset
+                        if (!(stopwatch.Elapsed.Seconds > 0))
+                            stopwatch.Start();
+                        // stopwatch.Stop();
+                        timer_label.Content = stopwatch.Elapsed;
+
                     }
                 }
                 catch (Exception ex)
                 {
-                    Upload_status.Foreground = Brushes.Red;
-                    Upload_status.Content = "Image failed to upload.";
+                    status_box.Foreground = Brushes.Red;
+                    status_box.Content = "Image failed to upload, Drone not started. ";
+
+                    status_label.Foreground = Brushes.Red;
+                    status_label.Content = "Inactive";
                 }
-                
-            } 
+
+            }
+            else
+            {
+
+            }
         }
 
+        // Sends signal to drone for the search to cease.
         private void Stop_Button1_Click(object sender, RoutedEventArgs e)
         {
             isSearching = false;
-            //TODO reset images;
-            RetrieveImage();
-        }
 
-        private static UI_Network comms = new UI_Network();
+            // Reset ImageBox images.
+            search_for_image.Source = null;
+            person_found.Source = null;
 
-        private void Start_Button_Click(object sender, RoutedEventArgs e)
-        {
+            // Sets ouput log to respective color and text.
+            status_box.Foreground = Brushes.Red;
+            status_box.Content = "Drone Stopped. ";
+
+            // Sets Drone Activity Label to respective color and status.
+            status_label.Foreground = Brushes.Red;
+            status_label.Content = "Inactive";
+
+            // RetrieveImage();
+
             try
             {
-                comms.SendStart();
-            }
-           catch
-            {
-               
-            }
-        }
-
-        private void Stop_Button_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-               comms.SendStop();
+                // comms.SendStop();
             }
             catch
             {
@@ -150,66 +194,93 @@ namespace WpfApp1
             }
         }
 
-        //forces the drone to stop
-        private void KillSwitch_Click(object sender, RoutedEventArgs e)
+        // Sends signal for drone to continue it's search and find a new target.
+        private void Continue_Searching_Click(object sender, RoutedEventArgs e)
         {
-             comms.SendKill();
-        }
-        
-        //disables the drone buttons if there is no person being searched for yet
-        private void Can_Start()
-        {
-            if (isSearching == false)
-            {
-                Start_Button.IsEnabled = false;
-                Start_Button.Content = "Please Select \n a target first.";
-                Stop_Button.IsEnabled = false;
-                Stop_Button.Content = "Please Select \n a target first.";
-            }
-            else
-            {
-                Start_Button.IsEnabled = true;
-                Start_Button.Content = "Start Drone";
-                Stop_Button.IsEnabled = false;
-                Stop_Button.Content = "Stop Drone";
-            }  
+            //TODO - make drone continue searching
+
+            status_box.Foreground = Brushes.Green;
+            status_box.Content = "continuing Search. ";
         }
 
+        // Forces the drone to stop.
+        private void KillSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            //  comms.SendKill();
+
+
+        }
+
+        // Gathers weather information from dedicated server, then displays to UI
+        // if it is safe to use the drone outside.
+        void Safe_to_Fly()
+        {
+            var connect = true;
+            var data = "";
+            var value = 0;
+
+            // Defaults displayed text to black.
+            safe_to_fly_status.Foreground = Brushes.Black;
+
+            XmlDocument pullWeather = new XmlDocument();
+
+            // Attempts to gather current weather information.
+            try
+            {
+                pullWeather.Load("http://api.openweathermap.org/data/2.5/weather?zip=03060,us&mode=xml&appid=f95d4be882833be32b011342f0b6abc5");
+
+                data = pullWeather.SelectSingleNode("/current/weather").Attributes[0].InnerText;
+            }
+            catch (Exception ex)
+            {
+                connect = false;
+
+                status_box.Foreground = Brushes.Red;
+                status_box.Content = "Failed to retrieve weather information. ";
+
+                safe_to_fly_status.Foreground = Brushes.Black;
+                safe_to_fly_status.Content = "Unknown";
+            }
+
+            // Converts gathered data to a int.
+            Int32.TryParse(data, out value);
+
+            // Determines safety level by determining if value is above or below 800.
+            if (value / 100 == 8)
+            {
+                safe_to_fly_status.Foreground = Brushes.Green;
+                safe_to_fly_status.Content = "Safe";
+            }
+            else if (connect != false)
+            {
+                safe_to_fly_status.Foreground = Brushes.Red;
+                safe_to_fly_status.Content = "Not Safe";
+            }
+            // Weather condition codes.
+            // http://openweathermap.org/weather-conditions
+
+
+        }
+
+        // When drone finds target, function sets ImageBox to UI.
         void RetrieveImage()
         {
+            var Network = new UI_Network();
+
             myImage = "/*set to the incoming image**/";
 
             person_found.Source = setImage();
         }
 
+        // Returns a usable image converted from OpenFileDialog sourcestream.
         BitmapImage setImage()
         {
-            BitmapImage b = new BitmapImage();
+            var b = new BitmapImage();
             b.BeginInit();
             b.UriSource = new Uri(myImage);
             b.EndInit();
             return b;
         }
 
-        void Safe_to_Fly()
-        {
-            string data;
-            int value;
-
-            XmlDocument pullWeather = new XmlDocument();
-            pullWeather.Load("http://api.openweathermap.org/data/2.5/weather?zip=03060,us&mode=xml&appid=f95d4be882833be32b011342f0b6abc5");
-            
-            data = pullWeather.SelectSingleNode("/current/weather").Attributes[0].InnerText;
-
-            Int32.TryParse(data, out value);
-
-            if (value / 100 == 8)
-            {
-                found_label_Copy.Content = "Safe";
-            }
-            else
-                found_label_Copy.Content = "Not Safe";
-            /*codes => http://openweathermap.org/weather-conditions */
-        }
     }
 }
